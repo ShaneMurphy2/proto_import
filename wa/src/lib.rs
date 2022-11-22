8a47f91507074b1eb8b003c9a23f10768e66b60e
Shane <shane.murphy@umbra.space>
2022-11-22 12:01:01 -0800

    Move wasm into src

--- wa/src/lib.rs
+++ wa/src/lib.rs
@@ -2,7 +2,7 @@ use proc_macro::TokenStream;
 use watt::WasmMacro;
 
 static MACRO: WasmMacro = WasmMacro::new(WASM);
-static WASM: &[u8] = include_bytes!("../../impl/target/wasm32-unknown-unknown/release/proto_import.wasm");
+static WASM: &[u8] = include_bytes!("proto_import.wasm");
 
 /// Import `prost` generated definitions.
 ///
